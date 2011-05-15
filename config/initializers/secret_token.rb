9a70cb45da943485fbe595ad283eb3de8f15451a
Nerian <siotopo@gmail.com>
2011-05-15 13:29:19 +0200

    Remove rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DPovray::Application.config.secret_token = '75559d76569df438ca3c3a154694650db240d432ae01b4c21cef352a84eaf0e29f49c190f0868b3b0f262d53804c59eb8e06b3102234f41918ff3acdbdc4dd51'
