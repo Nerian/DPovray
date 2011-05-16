23cc303c334798d9f23c3a1658d76b75ff578d01
Nerian <siotopo@gmail.com>
2011-05-16 18:47:02 +0200

    Cleanup database after each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 require 'rspec'
 require 'rack/test' 
 require 'dpovray'
+require 'resque_spec' 
 require 'capybara'   
 require 'capybara/rspec'   
 include Capybara::DSL
-require 'resque_spec'
 
-Capybara.app = DPovray::App                  
+Capybara.app = DPovray::App
 
 set :environment, :test 
 
@@ -15,5 +15,30 @@ def app
 end
 
 RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
-end                         
+  conf.include Rack::Test::Methods    
+  
+  conf.before(:each) do    
+    cleanup_redis    
+  end
+end
+
+def redis
+  Redis.new
+end
+
+def cleanup_redis
+  redis.flushdb  
+end
+         
+def send_a_scene
+  visit('/') 
+  page.fill_in('name', :with => 'Gondar')
+  page.fill_in('height', :with => '100')
+  page.fill_in('width', :with => '100')
+  page.attach_file('scene', File.dirname(__FILE__) + "/support/buckyball.pov")
+  page.click_button('submit')  
+end 
+
+def scene_file
+  File.read(File.dirname(__FILE__)+"/support/buckyball.pov")
+end
