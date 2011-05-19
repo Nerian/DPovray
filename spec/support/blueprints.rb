97354cdf373a71d24af6264aceff17ce8170fc3f
Nerian <siotopo@gmail.com>
2011-05-19 18:41:01 +0200

    use real scene file

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -2,7 +2,7 @@ require 'machinist'
 
 DPovray::Task.blueprint() do
   project { 1000 }          
-  povray_options { {'height'=>50, 'width'=>50, 'start_row'=>1, 'start_column'=>1, 'end_row'=>50, 'end_column'=>50, 'scene'=> 'scene_file' } }
+  povray_options { {'height'=>50, 'width'=>50, 'start_row'=>1, 'start_column'=>1, 'end_row'=>50, 'end_column'=>50, 'scene'=> scene_file } }
   order { '0' }
-  partial_image { scene_file }  
+  partial_image { nil }  
 end
