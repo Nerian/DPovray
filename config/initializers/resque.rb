745e3429a0b875d0b63d5f26dabcf481848d0655
Nerian <siotopo@gmail.com>
2011-05-25 18:10:56 +0200

    The initializer to configure redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,14 @@
+require 'yaml'
+
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 Resque.redis = resque_config[rails_env]
+
+begin          
+  Resque.redis.ping
+rescue Errno::ECONNREFUSED 
+  puts $!
+  exit
+end
